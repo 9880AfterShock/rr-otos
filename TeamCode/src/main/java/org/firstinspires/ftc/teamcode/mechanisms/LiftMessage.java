17459882806d84721814991cc04531c5cca3da31
cadenthecreator <cadendanielream@outlook.com>
2025-07-21 13:55:22 -0700

    STATE MACHINE STATE MACHINE TATE MACHINE STATE MACHINE

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/LiftMessage.java
@@ -0,0 +1,9 @@
+package org.firstinspires.ftc.teamcode.mechanisms;
+
+public enum LiftMessage {
+    RAISE,
+    LOWER,
+    STOP,
+    RESET,
+    NONE
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/LiftState.java
@@ -0,0 +1,8 @@
+package org.firstinspires.ftc.teamcode.mechanisms;
+
+public enum LiftState {
+    UP,
+    DOWN,
+    STOPPED,
+    RESETTING;
+}
